7794e01e9f4cb70260e875169c25fe05fedb8ccf
Jhansi <95856797+jhans1-cs@users.noreply.github.com>
2023-05-23 16:28:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 //your JS code here. If required.
-<script>
-        function changeColor() {
-            var blockId = document.getElementById('block_id').value;
-            var colorId = document.getElementById('color_id').value;
-            var block = document.getElementById(blockId);
-            
-            resetGrid();
-            block.style.backgroundColor = colorId;
-        }
-        
-        function resetGrid() {
-            var gridItems = document.getElementsByClassName('grid-item');
-            for (var i = 0; i < gridItems.length; i++) {
-                gridItems[i].style.backgroundColor = 'transparent';
-            }
-        }
-    </script>
